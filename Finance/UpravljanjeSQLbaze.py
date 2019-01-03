1e57dd71237ddbacad76fbb7e53c49b2e081e411
unknown <kriznar.p@gmail.com>
2019-01-03 23:39:11 +0100

    funkcija nastej_stolpce v UpravljanjeSQLbaze.py

--- Finance/UpravljanjeSQLbaze.py
+++ Finance/UpravljanjeSQLbaze.py
@@ -8,10 +8,13 @@ def preberi_tabelo(cursor,tabela):
     cursor.execute('SELECT * FROM '+tabela)
 def preberi_stolpec(cursor,tabela,stolpec):
     cursor.execute('SELECT '+stolpec+' FROM ' + tabela)
-
-preberi_tabelo(c,'Posel')
-preberi_stolpec(d,'Posel','PoselID,PoselIme')
-print(d.fetchone())
+def nastej_stolpce(cursor,tabela):
+    cursor.execute('PRAGMA table_info('+tabela+')')
+#preberi_tabelo(c,'Posel')
+#preberi_stolpec(d,'Posel','PoselID,PoselIme')
+#print(d.fetchone())
+#print(c.fetchall())
+#print(d.fetchone())
+#print(d.fetchone())
+nastej_stolpce(c,'Posel')
 print(c.fetchall())
-print(d.fetchone())
-print(d.fetchone())
